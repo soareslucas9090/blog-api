86eae48b4be001a8c7f4a584813e2b8a2be0a4ca
Lucas Soares <soareslukas9090@gmail.com>
2024-01-06 14:41:06 -0300

    IsAuthenticatedOrReadOnly

--- blog/permissions.py
+++ blog/permissions.py
@@ -3,4 +3,4 @@ from rest_framework import permissions
 
 class IsOwnerPost(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.author
+        return obj.author.user == request.user
